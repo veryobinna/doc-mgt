569f5c83ab8d506ed2cd3a727350547848d99b89
Obinna Okwuolisa <obinna.okwuolisa@andela.com>
2017-07-20 05:15:29 +0100

    update test

---
+++ server/test/client/index.spec.js
@@ -0,0 +1,43 @@
+import '../../../client/test/browser';
+
+import '../../../client/test/actions/AuthAction.spec';
+import '../../../client/test/actions/DocumentAction.spec';
+import '../../../client/test/actions/UserActions.spec';
+
+import '../../../client/test/components/Footer.spec';
+import '../../../client/test/components/Navbar.spec';
+import '../../../client/test/components/SearchBar.spec';
+import '../../../client/test/components/ShowDocument.spec';
+import '../../../client/test/components/ShowSingleDocument.spec';
+import '../../../client/test/components/ShowUsers.spec';
+
+import '../../../client/test/container/AddDocument.spec';
+import '../../../client/test/container/Dashboard.spec';
+import '../../../client/test/container/GetDocument.spec';
+import '../../../client/test/container/GetSingleDocument.spec';
+import '../../../client/test/container/GetUsers.spec';
+import '../../../client/test/container/Login.spec';
+import '../../../client/test/container/Signup.spec';
+import '../../../client/test/container/UpdateDocument.spec';
+import '../../../client/test/container/UpdateUser.spec';
+
+import '../../../client/test/reducers/AuthReducer.spec';
+import '../../../client/test/reducers/DocumentReducer.spec';
+import '../../../client/test/reducers/UserReducer.spec';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
